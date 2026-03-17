00ea10050d8b2302ddbb3dba921eb404407ca006
Danil <danilalexeev162@gmail.com>
2026-03-17 21:42:19 +0500

    add homework1_1-5

--- src/homework_1/Homework_1_4.java
+++ src/homework_1/Homework_1_4.java
@@ -3,7 +3,7 @@ package homework_1;
 public class Homework_1_4 {
     public static void main(String[] args) {
         double n = 75.5478;
-        int a = (int) n;
+        int a = (int)  Math.round(n);
         System.out.println(a);
     }
 }
